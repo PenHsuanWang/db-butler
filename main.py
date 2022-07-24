0f0aab788c459760fe9bf2a42f4f65cbdb011689
Pen-Hsuan Wang <willy118116@gmail.com>
2022-07-24 15:16:45 +0800

    testing search with filter

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ if __name__ == '__main__':
         password='pwang'
     )
 
-    data = {"name": "pwang", "age": 28, "address": {"street": "松仁路七號", "city": "台北市", "country": "Taiwan"}}
+    # data = {"name": "pwang", "age": 28, "address": {"street": "松仁路七號", "city": "台北市", "country": "Taiwan"}}
     # mongo_butler.save_data(data=data, db_name="python_sdk_test", collection_name="person")
-    mongo_butler.find_data_and_print(db_name="python_sdk_test", collection_name="person")
+    # data = mongo_butler.find_one_data(db_name="youbike_database", collection_name="taipei_youbike_records")
+    mongo_butler.find_data_and_print(db_name="youbike_database", collection_name="taipei_youbike_records")
+    # print(data)
